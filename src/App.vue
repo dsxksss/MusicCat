8ee990934ed0159662f756c9a82636132c9451d2
VentCat <2546650292@qq.com>
2023-07-03 01:18:41 +0800

    优化代码逻辑

--- src/App.vue
+++ src/App.vue
@@ -1,155 +1,104 @@
 <script setup>
 import Container from './Container.vue';
 import { ref, provide, onMounted, onUnmounted, watch } from "vue"
+import { Howl } from 'howler';
 
 let timer = null
+let sound = ref(null)
 const startTime = ref(0)
 const elapsedTime = ref(0)
 const totalTime = ref(0)
-let audioContext = null
-let audioBuffer = null
-let sourceNode = null
-let fileReader = new FileReader()
+const formatElapsedTime = ref("")
+const formatTotalTime = ref("")
 const isPlaying = ref(false)
 
-const startAudioContext = () => {
-  audioContext = new AudioContext()
-}
-
-const stopAudioContext = () => {
-  audioContext.close()
-  audioContext = null
-}
-
-const loadAudioFile = file => {
-  fileReader.readAsArrayBuffer(file)
-
-  fileReader.onloadend = () => {
-    if (audioContext) {
-      audioContext.decodeAudioData(fileReader.result, buffer => {
-        audioBuffer = buffer
-        totalTime.value = audioBuffer.duration
-      })
-    }
-  }
-}
-
-const playAudio = () => {
-  if (audioContext) {
-    startAudioContext()
-  }
-
-  if (audioBuffer) {
-    sourceNode = audioContext.createBufferSource()
-    sourceNode.buffer = audioBuffer
-    sourceNode.connect(audioContext.destination)
-    sourceNode.start(0, elapsedTime.value % totalTime.value)
-    audioContext.resume()
-    isPlaying.value = true
-
-    timer = setInterval(() => {
-      updateTime()
-    }, 1000)
-  }
-}
-
-const pauseAudio = () => {
-  if (sourceNode) {
-    sourceNode.stop(0)
-    sourceNode = null
-    audioContext.suspend()
-    isPlaying.value = false
-
-    clearInterval(timer)
-  }
-}
-
-const stopAudio = () => {
-  clearInterval(timer)
-  if (sourceNode) {
-    sourceNode.stop(0)
-    sourceNode = null
-    audioContext.suspend()
-
-    elapsedTime.value = 0
-    startTime.value = 0
-    isPlaying.value = false
-
-    stopAudioContext()
-    startAudioContext()
-  }
-}
-
-const setAudioPlayTime = (time) => {
-  clearInterval(timer);
-
-  if (sourceNode && audioBuffer) {
-    const newTime = time % totalTime.value;
-
-    if (newTime < 0) {
-      elapsedTime.value = Math.abs(newTime);
-    } else {
-      elapsedTime.value = newTime;
-    }
-
-    const newStartTime = audioContext.currentTime - newTime;
-
-    stopAudioContext()
-    startAudioContext()
-
-    if (newStartTime < 0) {
-      startTime.value = Math.abs(newStartTime);
-    } else {
-      startTime.value = newStartTime;
-    }
-    sourceNode.stop(0);
-    sourceNode = audioContext.createBufferSource();
-    sourceNode.buffer = audioBuffer;
-    sourceNode.connect(audioContext.destination);
-    sourceNode.start(0, newTime);
-    audioContext.resume();
-    timer = setInterval(() => {
-      updateTime();
-    }, 1000);
-  }
-};
-
-const updateTime = () => {
-  const newElapsedTime = audioContext.currentTime + startTime.value
-
-
-  if (isPlaying.value && audioBuffer) {
-    if (newElapsedTime < 0) {
-      elapsedTime.value = Math.abs(newElapsedTime)
-    } else {
-      elapsedTime.value = newElapsedTime;
-    }
-
-    if (elapsedTime.value >= totalTime.value) {
-      stopAudio()
-    }
-
-  }
-}
 
 const onFileChange = event => {
-  const files = event.target.files
-  if (files.length > 0) {
-    loadAudioFile(files[0])
+  if (event.target.files.length > 0) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.addEventListener('load', function () {
+      const data = reader.result;
+      sound.value = new Howl({
+        src: data,
+        loop: false,
+        format: file.name.split('.').pop().toLowerCase(),
+      });
+    });
+    reader.readAsDataURL(file);
+  };
+}
+
+function playAudio() {
+  sound.value.play()
+  updateStatus()
+  updateTime()
+}
+
+function pauseAudio() {
+  clearTimer()
+  sound.value.pause()
+  updateStatus()
+}
+
+function setAudioPlayTime(seek){
+  sound.value.stop()
+  sound.value.seek(seek)
+  sound.value.play()
+  updateStatus()
+}
+
+function stopAudio() {
+  clearTimer()
+  sound.value.stop()
+  updateStatus()
+}
+
+function updateTime() {
+  if(!timer){
+    timer = setInterval(()=>{
+      if(!sound.value.playing() && sound.value.seek() === 0){
+        console.log("计时器停止");
+        stopAudio()
+      }else{
+        updateStatus()
+      }
+    },1000)
   }
 }
+
+function updateStatus(){
+  totalTime.value = sound.value.duration();
+  elapsedTime.value = sound.value.seek()
+  formatTotalTime.value = formatTime(Math.round(sound.value.duration()));
+  formatElapsedTime.value = formatTime(Math.round(sound.value.seek()));
+  isPlaying.value = sound.value.playing()
+}
+
+function clearTimer(){
+  clearInterval(timer)
+  timer = null
+}
+
+function formatTime(secs) {
+  var minutes = Math.floor(secs / 60) || 0;
+  var seconds = (secs - minutes * 60) || 0;
+
+  return minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
+}
+
 provide("isPlaying", isPlaying)
 provide("startTime", startTime)
 provide("elapsedTime", elapsedTime)
 provide("totalTime", totalTime)
+provide("formatElapsedTime", formatElapsedTime)
+provide("formatTotalTime", formatTotalTime)
 provide("playAudio", playAudio)
 provide("pauseAudio", pauseAudio)
 provide("stopAudio", stopAudio)
-provide("setAudioPlayTime", setAudioPlayTime)
 provide("updateTime", updateTime)
+provide("setAudioPlayTime", setAudioPlayTime)
 
-onMounted(startAudioContext)
-onUnmounted(stopAudioContext)
 
 watch(startTime, () => {
   console.log("开始时间，发生变化:", startTime.value);
@@ -157,15 +106,22 @@ watch(startTime, () => {
 watch(elapsedTime, () => {
   console.log("已用时间，发生变化:", elapsedTime.value);
 })
+watch(formatElapsedTime, () => {
+  console.log("格式化已用时间，发生变化:", formatElapsedTime.value);
+})
 watch(totalTime, () => {
   console.log("总时间，发生变化:", totalTime.value);
+})
+watch(formatTotalTime, () => {
+  console.log("格式化总时间，发生变化:", formatTotalTime.value);
 })
 
 </script>
 
 <template>
   <div class="transition-all h-screen w-screen flex justify-center items-center">
-    <input v-if="totalTime <= 0" id="load" type="file" @change="onFileChange" accept=".mp3,.aac,.m4a" />
+    <input v-if="sound == null" class="file-input file-input-bordered file-input-success w-full max-w-xs" id="load"
+      type="file" @change="onFileChange" accept=".mp3,.aac,.m4a" />
     <Container v-else />
   </div>
 </template>
