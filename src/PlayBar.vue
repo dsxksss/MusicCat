c526cbfd39d2bd6c23ec303573ef6bb10b20d7aa
VentCat <2546650292@qq.com>
2023-07-03 12:58:58 +0800

    进度条时间字体调整适配 进度条长度适配

--- src/PlayBar.vue
+++ src/PlayBar.vue
@@ -14,13 +14,13 @@ const onRangeChange = event => {
 
 <template>
     <div class="h-[25%]">
-        <div class="flex justify-center items-center space-y-1 flex-col">
+        <div class="flex justify-center items-center space-y-1 sm:space-y-2 flex-col">
             <div class="flex w-[100%] justify-between mt-1 px-8">
                 <div></div>
                 <div class="text-lg font-bold text-gray-300/60">{{ formatTotalTime }}</div>
             </div>
             <input type="range" @change="onRangeChange" min=0 :max=totalTime :value=elapsedTime>
-            <div class="flex w-[100%] justify-between px-8 pt-1">
+            <div class="flex w-[100%] justify-between px-8 pt-1 sm:pt-2">
                 <div class="text-lg font-bold text-gray-300/60">{{ formatElapsedTime }}</div>
                 <div></div>
             </div>
@@ -38,6 +38,7 @@ input[type="range"] {
     background-size: 0% 100%;
     background-image: linear-gradient(#38BDF8, #38BDF8);
     background-repeat: no-repeat;
+    @apply sm:w-[84%] md:w-[85%];
 }
 
 input[type="range"]::-webkit-slider-thumb {
